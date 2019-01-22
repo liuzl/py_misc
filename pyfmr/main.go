49852125413a8ef98b29de431d5dd854e9f4f1ae
Zhanliang Liu <zhanliangliu@qq.com>
2019-01-22 17:30:22 +0800

    handle

--- pyfmr/main.go
+++ pyfmr/main.go
@@ -7,23 +7,41 @@ import "C"
 
 import (
 	"fmt"
+	"sync"
 	"unsafe"
 
 	"github.com/liuzl/fmr"
 )
 
-var g *fmr.Grammar
+var (
+	index    = make(map[string]int)
+	mutex    sync.RWMutex
+	grammars []*fmr.Grammar
+	g        *fmr.Grammar
+)
 
 //export init_grammar
-func init_grammar(s *C.char) {
+func init_grammar(s *C.char) int {
 	f := C.GoString(s)
+	mutex.RLock()
+	defer mutex.RUnlock()
+	if i, has := index[f]; has {
+		g = grammars[i]
+		return i
+	}
+
+	i := len(grammars)
+
 	var err error
 	g, err = fmr.GrammarFromFile(f)
 	if err != nil {
 		fmt.Println(f, " ", err.Error())
 		panic(err)
 	}
+	grammars = append(grammars, g)
+	index[f] = i
 	//fmt.Printf("grammar file loaded from %s\n", f)
+	return i
 }
 
 //export extract
@@ -31,6 +49,30 @@ func extract(l, s *C.char) *C.char {
 	line := C.GoString(l)
 	start := C.GoString(s)
 	trees, err := g.ExtractMaxAll(line, start)
+	if err != nil {
+		return C.CString(err.Error())
+	}
+	for _, tree := range trees {
+		sem, err := tree.Semantic()
+		if err != nil {
+			return C.CString(err.Error())
+		} else {
+			// return the first one
+			return C.CString(sem)
+		}
+	}
+	return C.CString("no result")
+}
+
+//export extractx
+func extractx(i int, l, s *C.char) *C.char {
+	if i < 0 || i >= len(grammars) {
+		return C.CString("no grammar")
+	}
+	lg := grammars[i]
+	line := C.GoString(l)
+	start := C.GoString(s)
+	trees, err := lg.ExtractMaxAll(line, start)
 	if err != nil {
 		return C.CString(err.Error())
 	}
--- pyfmr/run.py
+++ pyfmr/run.py
@@ -3,23 +3,25 @@ fmr = ctypes.CDLL('./fmr.so')
 charptr = ctypes.POINTER(ctypes.c_char)
 
 fmr.extract.restype = charptr
+fmr.extractx.restype = charptr
 
 def c(s): return ctypes.create_string_buffer(s.encode('utf-8'))
 
 g = c("./sf.grammar")
 s = c("cities")
 
-fmr.init_grammar(g)
+i = fmr.init_grammar(g)
 
 strs = [
     "直辖市：北京、上海、天津",
     "直辖市：帝都、津城、魔都",
     "中国现在有四个直辖市：帝都、魔都、天津、重庆。",
     "中国曾经的直辖市：帝都、魔都、天津、重庆、旧都。",
+    "天津大学",
 ]
 
 for l in strs:
-    ret = fmr.extract(c(l), s)
+    ret = fmr.extractx(i, c(l), s)
     value = ctypes.cast(ret, ctypes.c_char_p).value
     fmr.gofree(ret)
     print(value.decode('utf-8'))
