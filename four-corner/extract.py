3d43cf75418158489e43b42098dfe48dbfee121f
liuzhanliang <zhanliangliu@qq.com>
2019-08-23 20:03:03 +0800

    code,text -> text,code

--- four-corner/extract.py
+++ four-corner/extract.py
@@ -1,7 +1,7 @@
 import pickle
 data =  pickle.load(open("data.pkl", 'rb'))
 out = open("four.csv", "wb")
-out.write(b"code,text\n")
+out.write(b"text,code\n")
 for k, v in data.items():
     line = "%s,%s\n" % (k, v)
     out.write(line.encode('utf-8', 'ignore'))
