d1fb92708196911f9efde4ec7f814a76a1ae85d3
liuzhanliang <zhanliangliu@qq.com>
2019-12-02 09:47:05 +0800

    add wordninja_example.py

---
+++ word/wordninja_example.py
@@ -0,0 +1,13 @@
+import wordninja
+
+cases = [
+    "derekanderson",
+    "imateapot",
+    "heshotwhointhewhatnow",
+    "helloworld!",
+    "thequickbrownfoxjumpsoverthelazydog",
+    "thisisours",
+]
+
+for c in cases:
+    print(wordninja.split(c))
