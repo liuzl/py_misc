04a6f84a9d7ebb5e15508dd864b2a6de9e6ed903
liuzhanliang <zhanliangliu@qq.com>
2020-02-09 17:09:45 +0800

    update server.py

--- pyserver/server.py
+++ pyserver/server.py
@@ -3,12 +3,14 @@ import json
 from flask import Flask, request, Response
 app = Flask(__name__, static_folder='ui', static_url_path='')
 
-@app.route("/api")
+@app.route("/api", methods=['GET', 'POST'])
 def get():
-    r = request.args.get('text', '')
-    if r == "":
+    if request.method == "POST": r = request.form
+    else: r = request.args
+    t = r.get('text', '')
+    if t == "":
         return Response(json.dumps({'status':"error", 'message':"empty input"}))
-    return Response(json.dumps({'status':"ok", 'message':"hello"}),
+    return Response(json.dumps({'status':"ok", 'message':t}, ensure_ascii=False),
             mimetype="application/json")
 
 
