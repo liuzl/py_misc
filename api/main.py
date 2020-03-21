1e3ce808cad9a239d59b733c93e5b1b71a353383
liuzhanliang <zhanliangliu@qq.com>
2020-03-21 18:54:09 +0800

    update main.py

--- api/main.py
+++ api/main.py
@@ -5,3 +5,13 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'message': '你已经正确创建FastApi服务！'}
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get('/item/')
+def get_item(skip: int=0, limit: int=10):
+    return fake_items_db[skip: skip+limit]
