a84cf0ccced7e92ae05f180212a71ff462820d75
liuzhanliang <zhanliangliu@qq.com>
2020-04-19 13:34:44 +0800

    update pbc.py

--- requests-html/pbc.py
+++ requests-html/pbc.py
@@ -1,7 +1,19 @@
+import urllib.parse
 from requests_html import HTMLSession
 url = 'http://www.pbc.gov.cn/goutongjiaoliu/113456/113469/index.html'
-session = HTMLSession()
-r = session.get(url)
-r.html.render(sleep=1, keep_page=True)
-for node in r.html.find('font.newslist_style > a'):
-    print(list(node.absolute_links)[0], node.text)
+data = []
+while True:
+    print(url)
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render(sleep=1, keep_page=True)
+    for node in r.html.find('font.newslist_style > a'):
+        data.append({'url':list(node.absolute_links)[0], 'title':node.text})
+    link = r.html.find("a:contains('下一页')")
+    if len(link) <= 0:
+        break
+    url = urllib.parse.urljoin(link[0].base_url, link[0].attrs['tagname'])
+    session.close()
+import pandas as pd
+df = pd.DataFrame(data)
+df.to_csv("pbc_news.csv", index=None)
