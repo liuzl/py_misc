03b8bac35dc4dc2a86130641ed8fb002770e1946
liuzl <liang@zliu.org>
2020-06-12 07:55:43 +0000

    update main.py

--- corrector/main.py
+++ corrector/main.py
@@ -1,4 +1,6 @@
 import pycorrector
+import sys
 s = '少先队员因该为老人让坐'
-ret, detail = pycorrector.correct(s)
-print(ret, detail)
+for s in sys.stdin:
+    ret, detail = pycorrector.correct(s)
+    print(ret, detail)
