b7150f7cdb279805acdf6c47a6a6f8deca350129
liuzl <liang@zliu.org>
2021-02-14 19:55:39 +0800

    update check.py

--- playwright-learn/check.py
+++ playwright-learn/check.py
@@ -1,6 +1,13 @@
 from playwright.sync_api import sync_playwright
 import time
 
+def check(page, text, img):
+    page.fill("textarea[placeholder=\"请输入您的问题，Shift+Enter换行\"]", text)
+    page.press("textarea[placeholder=\"请输入您的问题，Shift+Enter换行\"]", "Enter")
+    time.sleep(2)
+    page.screenshot(path=img)
+
+
 def run(playwright):
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
@@ -11,15 +18,9 @@ def run(playwright):
     # Go to https://admin.iir.ac.cn/result?id=75
     page.goto("https://admin.iir.ac.cn/result?id=75")
 
+    check(page, "一老一小怎么办理？", "yilaoyixiao.png")
+    check(page, "高新技术企业认定和高新技术产品认定是一回事吗？", "gaoxin.png")
 
-    # Fill textarea[placeholder="请输入您的问题，Shift+Enter换行"]
-    page.fill("textarea[placeholder=\"请输入您的问题，Shift+Enter换行\"]", "一老一小怎么办理？")
-
-    # Press Enter
-    page.press("textarea[placeholder=\"请输入您的问题，Shift+Enter换行\"]", "Enter")
-    time.sleep(2)
-    page.screenshot(path="name.png")
-    # ---------------------
     context.close()
     browser.close()
 
