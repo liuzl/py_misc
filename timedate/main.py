7480b8ab681d6f0c55b34e5a0b8d7f55cf57ad4c
liuzl <liang@zliu.org>
2022-01-19 10:53:37 +0800

    this_month, last_month

--- timedate/main.py
+++ timedate/main.py
@@ -1,6 +1,11 @@
 # pip install python-dateutil
 
-from datetime import *; from dateutil.relativedelta import *
+from datetime import *
+from dateutil.relativedelta import *
 
-x = datetime.today()+relativedelta(months=+1)
-print(x)
+today = datetime.today()
+
+this_month = datetime(today.year, today.month, 1)
+print(this_month)
+last_month = this_month + relativedelta(months=-1)
+print(last_month)
