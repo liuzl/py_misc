1d43f5e8b59ae78a1ce149cbee01e046932ab612
Zhanliang Liu <liang@zliu.org>
2022-08-08 09:57:28 +0800

    current dir

--- files/allfiles.py
+++ files/allfiles.py
@@ -1,6 +1,7 @@
-import glob
+import glob, os
 from datetime import datetime
+p = os.path.dirname(os.path.realpath(__file__))
 with open(f"{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as out:
-    for f in glob.glob("**", recursive=True):
+    for f in glob.glob(f"{p}{os.sep}**", recursive=True):
         out.write(f"{f}\n")
 
