f71d79bfcecf6a73348f59b5628536466162bbca
Zhanliang Liu <liang@zliu.org>
2022-08-24 20:54:06 +0800

    utf-8 encode

--- files/allfiles.py
+++ files/allfiles.py
@@ -7,11 +7,12 @@ else:
     p = os.path.dirname(os.path.abspath(__file__))
 p = p + os.sep
 print(p)
-with open(f"{p}{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as out:
+with open(f"{p}{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "wb") as out:
     for f in glob.glob(f"{p}**", recursive=True):
         if f.startswith(p):
             f = f[len(p):]
         if f == "": continue
+        f = (f+"\n").encode("utf-8")
         print(f)
-        out.write(f"{f}\n")
+        out.write(f)
 
