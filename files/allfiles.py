0666920ad8cabf18fcc496067e5df92b069a2a04
Zhanliang Liu <liang@zliu.org>
2022-08-31 11:57:09 +0800

    update allfiles.py

--- files/allfiles.py
+++ files/allfiles.py
@@ -9,10 +9,12 @@ p = p + os.sep
 print(p)
 with open(f"{p}{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "wb") as out:
     for f in glob.glob(f"{p}**", recursive=True):
+        if os.path.isdir(f):
+            continue
         if f.startswith(p):
             f = f[len(p):]
         if f == "": continue
-        f = (f+"\n").encode("utf-8")
         print(f)
+        f = (f+"\r\n").encode("utf-8")
         out.write(f)
 
