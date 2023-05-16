1a7816adf81051006b62f830c4f3529a38fdc6ec
Zhanliang Liu <liang@zliu.org>
2023-05-16 10:48:40 +0800

    using python=dotenv

--- azureopenai/main.py
+++ azureopenai/main.py
@@ -2,6 +2,10 @@
 #Note: The openai-python library support for Azure OpenAI is in preview.
 import os
 import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
 openai.api_type = "azure"
 openai.api_base = os.getenv("API_BASE")
 openai.api_version = os.getenv("API_VERSION")
@@ -9,7 +13,7 @@ openai.api_key = os.getenv("API_KEY")
 
 response = openai.ChatCompletion.create(
   engine=os.getenv("ENGINE"),
-  messages = [{"role":"user","content":"你好"},],
+  messages = [{"role":"user","content":"Write en poem about Summer"},],
   temperature=0.5,
   max_tokens=800,
   top_p=0.95,
