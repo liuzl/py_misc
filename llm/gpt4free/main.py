c5cc9a55eca37fbb531ae6d2c995815a3b032667
Zhanliang Liu <liang@zliu.org>
2023-10-05 17:22:54 +0800

    g4f

---
+++ llm/gpt4free/main.py
@@ -0,0 +1,16 @@
+import g4f
+
+
+print(g4f.Provider.Ails.params)  # supported args
+
+# Automatic selection of provider
+
+# streamed completion
+response = g4f.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello world"}],
+    stream=True,
+)
+
+for message in response:
+    print(message, flush=True, end='')
