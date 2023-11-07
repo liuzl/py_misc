930118301db47cfb3ca7dc8529e4011a878b9345
Zhanliang Liu <liang@zliu.org>
2023-11-07 11:07:01 +0800

    tts_main

---
+++ llm/openai/openai1106/tts_main.py
@@ -0,0 +1,31 @@
+import io
+from openai import OpenAI
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+client = OpenAI()
+
+def stream_and_play(text):
+  response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input=text,
+  )
+
+  # Convert the binary response content to a byte stream
+  byte_stream = io.BytesIO(response.content)
+
+  # Read the audio data from the byte stream
+  audio = AudioSegment.from_file(byte_stream, format="mp3")
+
+  # Play the audio
+  play(audio)
+
+
+if __name__ == "__main__":
+  text = input("Enter text: ")
+  stream_and_play(text)
