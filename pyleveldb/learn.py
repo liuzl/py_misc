c7cc28ccab6aad89fab39cd5c66ff2ab10b879ca
Zhanliang Liu <liang@zliu.org>
2024-02-14 14:16:32 +0800

    Updated sessions database and added history feature

--- pyleveldb/learn.py
+++ pyleveldb/learn.py
@@ -1,15 +1,25 @@
 from lsm import LSM
+import time
 
-sessions = LSM('sessions')
+sessions = LSM('sessions.db')
 
 user_id = 1234567
 model_key = "model"
 voice_key = "voice"
+history_key = "history"
 
-key = f"{user_id}:{model_key}"
-value = "gemini-pro"
+#sessions[f"{user_id}:{model_key}"] = "gemini-pro"
+#sessions[f"{user_id}:{voice_key}"] = "zliu"
 
-sessions[key] = value
+now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-for k, v in sessions:
+#s = "this is just a test"
+#sessions[f"{user_id}:{history_key}:{now_str}"] = s
+
+for k, v in sessions[f"{user_id}:{history_key}:":f"{user_id}:{history_key}:{now_str}"]:
+    print(k, v)
+
+print("="*20)
+
+for k, v in sessions[f"{user_id}:{history_key}:{now_str}":f"{user_id}:{history_key}:"]:
     print(k, v)
