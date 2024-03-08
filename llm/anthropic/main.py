a8fd20575c2948bf373febe8f517c4a47d5023b5
liuzl <liang@zliu.org>
2024-03-08 14:52:01 +0800

    env ANTHROPIC_BASE_URL

--- llm/anthropic/main.py
+++ llm/anthropic/main.py
@@ -5,12 +5,13 @@ _ = load_dotenv(find_dotenv())
 
 client = anthropic.Anthropic(
     api_key=os.getenv("ANTHROPIC_API_KEY"),
+    base_url=os.getenv("ANTHROPIC_BASE_URL"),
 )
 message = client.messages.create(
     model="claude-3-opus-20240229",
     max_tokens=1024,
     messages=[
-        {"role": "user", "content": "Hello, Claude"}
+        {"role": "user", "content": "Hello, Claude! Please response in Chinese."}
     ]
 )
 print(message.content)
